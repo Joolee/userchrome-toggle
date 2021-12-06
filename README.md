afa83af9c68773118a4e595bff67ba65e0ad9fcc
Joolee <570302+Joolee@users.noreply.github.com>
2021-12-06 22:39:48 +0100

    Initial commit
    Signed-off-by: Joolee <570302+Joolee@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# Userchrome Toggle
+<img align="right" src="https://github.com/Joolee/userchrome-toggle/raw/main/images/icon.svg" width="175">
+
+The userchrome.css file lets you customize the Firefox interface deeply, but it's not very interactive by default.
+This [Firefox extension](https://addons.mozilla.org/firefox/addon/userchrome-toggle/) adds a new button to your toolbar that allows you to toggle custom userchrome.css styling on-the fly by changing the window title. Using invisible UTF-8 characters by default, you won't even notice a thing. Toggle a single style or select up to three styles to toggle in a drop-down menu. Hardcore keyboard users can even use customizable keyboard-shortcuts.
+
+# Get help
+* Report issues or feature requests in the [bug tracker](https://github.com/Joolee/userchrome-toggle/issues)
---
+++ popup/popup.js
@@ -0,0 +1,47 @@
+'use strict';
+
+async function documentLoaded() {
+    document.getElementById('settings-link').addEventListener('click', event => {
+		browser.runtime.openOptionsPage();
+        window.close();
+        return false
+    })
+
+
+    const settings = await browser.storage.local.get(['toggles', 'general']);
+    const toggles = settings.toggles;
+    const type = settings.general.allowMultiple ? 'checkbox' : 'radio';
+
+    for (let i=0; i < toggles.length; i++) {
+        if ( ! toggles[i].enabled)
+            continue;
+
+        addSelectRow(type, i+1, toggles[i].name)
+    }
+
+    if (type == "radio") {
+        addSelectRow(type, 'none', 'None')
+    }
+}
+
+function addSelectRow(type, styleId, name) {
+    let container = document.createElement('div');
+    let selector = document.createElement('input');
+    container.appendChild(selector);
+    selector.type = type;
+    selector.id = `select-${styleId}`;
+    selector.name = 'style';
+    selector.addEventListener('change', event => {
+        console.log('Changed', event.target.id);
+        browser.extension.getBackgroundPage().userToggle(event.target.id, event.target.checked);
+    });
+
+    let label = document.createElement('label');
+    container.appendChild(label);
+    label.htmlFor = `select-${styleId}`;
+    label.textContent = name;
+
+    document.getElementById('content').appendChild(container);
+
+}
+document.addEventListener('DOMContentLoaded', documentLoaded);
